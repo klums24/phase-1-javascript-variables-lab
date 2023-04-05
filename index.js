e3bb1b293adcfe7df30b885d15d1946279337b4e
Kevin <klums24@gmail.com>
2023-04-05 15:44:03 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+
+//companyName is set to Scuber as a const
+const companyName = "Scuber";
+
+//mostProfitableNeighborhood is as equal to Chelsea. Define using let
+let mostProfitableNeighborhood = "Chelsea";
+
+//companyCeo is as equal to Susan Smith. Define using let
+let companyCeo = "Susan Smith";
